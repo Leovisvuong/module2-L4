1f6e063a3aeb0458d27305249899b38d6260661c
Leovisvuong <141708660+Leovisvuong@users.noreply.github.com>
2024-09-22 10:21:34 +0700

    Q2-Array

--- Program.cs
+++ Program.cs
@@ -1,84 +1,36 @@
-﻿// // Q1-List
-// // var declaration
-// string nameInput;
+﻿// // Q2-Array
 // // part 1
-// List<string> names = new List<string> {};
-// names.Add("Leovis");
-// names.Add("John");
-// names.Add("Anna");
-// names.Add("Elize");
-// names.Add("Don");
-// foreach (string name in names){
-//     Console.WriteLine(name);
-// }
+// string[] countries = new string[5] {"Japan","Korea","Brunei","Chile","Egypt"};
+// foreach(string countriesName in countries) Console.WriteLine(countriesName);
 
 // // part 2
-// Console.WriteLine(names[2]);
+// Console.WriteLine(countries[2]);
 
 // // part 3
-// names.RemoveAt(1);
-// foreach (string name in names){
-//     Console.WriteLine(name);
-// }
+// countries[1] = null;
+// foreach(string countriesName in countries) Console.WriteLine(countriesName);
 
 // // part 4
-// names.Add("Curshy");
-// foreach (string name in names){
-//     Console.WriteLine(name);
-// }
+// countries[2]="VietNam";
+// foreach(string countriesName in countries) Console.WriteLine(countriesName);
 
 // // part 5
-// Console.WriteLine(names.Count());
+// Console.WriteLine(countries.Count());
 
 // // part 6
-// Console.Write("Type a name: ");
-// nameInput = Console.ReadLine();
-// if(names.Contains(nameInput)){
-//     Console.WriteLine("YES");
-// }
+// Console.Write("Enter a name of a country that you want to check if it existed or not in the list: ");
+// string inputCountryName = Console.ReadLine();
+// if(countries.Contains(inputCountryName)) Console.WriteLine("YES");
 // else Console.WriteLine("NO");
 
 // // part 7
-// Console.Write("Type an exist name in the list: ");
-// nameInput = Console.ReadLine();
-// while(!names.Contains(nameInput)){
-//     Console.Write("Type an exist name in the list: ");
-//     nameInput = Console.ReadLine();
+// Console.Write("Enter an existed name in the list: ");
+// inputCountryName = Console.ReadLine();
+// while(!countries.Contains(inputCountryName)){
+//     Console.Write("Enter an existed name in the list: ");
+//     inputCountryName = Console.ReadLine();
 // }
-// Console.WriteLine(names.IndexOf(nameInput) + 1);
-
-// // part 8
-// names.Insert(2,"Ken");
-// foreach (string name in names){
-//     Console.WriteLine(name);
-// }
-
-// // part 9
-// names.Sort();
-// foreach (string name in names){
-//     Console.WriteLine(name);
-// }
-
-// // part 10
-// Console.WriteLine(names[4]);
-
-// // part 11
-// names.Clear();
-// foreach (string name in names){
-//     Console.WriteLine(name);
-// }
-
-// // part 12
-// List<int> ages = new List<int>() {};
-// names.Add("Lily");
-// ages.Add(17);
-// names.Add("Lucifer");
-// ages.Add(18);
-// foreach(string name in names){
-//     Console.WriteLine(name + " " + ages[names.IndexOf(name)]);
-// }
-
-
+// Console.WriteLine(Array.IndexOf(countries,inputCountryName) + 1);
 
 
 
